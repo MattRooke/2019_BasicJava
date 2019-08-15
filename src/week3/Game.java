a88d977c6c0bbfd6c98ac44442c9c54bfccb87b5
Matt Rooke <matthew.rooke@my.jcu.edu.au>
2019-08-16 00:19:00 +1000

    Added code for main method, allowing the game to be played a single time.

--- src/week3/Game.java
+++ src/week3/Game.java
@@ -1,6 +1,7 @@
 package week3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
     int count;
@@ -13,5 +14,21 @@ public class Game {
         if (value == secret) {
             wins += 1;
         }
+    }
+
+    public static void main(String[] args) {
+        Game guessingGame = new Game();
+        guessingGame.makeGuess(guessingGame.userInput());
+        if (guessingGame.wins == 1) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("Try Again!");
+        }
+    }
+
+    private int userInput() {
+        System.out.println("Guess a number from 1 to 10...");
+        Scanner userInput = new Scanner(System.in);
+        return userInput.nextInt();
     }
 }
