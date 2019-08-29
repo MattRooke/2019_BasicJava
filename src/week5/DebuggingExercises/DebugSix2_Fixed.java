05bd8bc95c053ae589ced87ca1cb56cfac49dbf8
Matthew Rooke <matthew.rooke@my.jcu.edu.au>
2019-08-29 22:15:33 +1000

    Simplify line separation.

--- src/week5/DebuggingExercises/DebugSix2_Fixed.java
+++ src/week5/DebuggingExercises/DebugSix2_Fixed.java
@@ -6,20 +6,17 @@ package week5.DebuggingExercises;
 public class DebugSix2_Fixed {
     final static int MIN = 65;
     final static int MAX = 122;
-
+    final static int LINES = 20;
     public static void main(String[] args) {
+        int currentLine = 1;
         for (int a = MIN; a <= MAX; a++) {
             char letter = (char) a;
             System.out.print("  " + letter);
-            if ((stopLines(20, a))) {
+            if (currentLine % LINES == 0) {
                 System.out.println();
             }
+            currentLine++;
         }
         System.out.println("\nEnd of application");
-    }
-
-    //Method finds when desired number of chars have been printed
-    public static boolean stopLines(int lines, int currentLine) {
-        return ((currentLine + 1) - MIN) % (lines) == 0;
     }
 }
