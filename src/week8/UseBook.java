a24c7913475bdea021499ef81d446af1efedb79a
Matthew Rooke <matthew.rooke@my.jcu.edu.au>
2019-09-19 15:24:12 +1000

    Simplify Sys.out statements

--- src/week8/UseBook.java
+++ src/week8/UseBook.java
@@ -4,7 +4,7 @@ public class UseBook {
     public static void main(String[] args) {
         Fiction book1 = new Fiction("The Dark Crystal");
         NonFiction book2 = new NonFiction("Science 101");
-        System.out.println(book1.toString());
-        System.out.println(book2.toString());
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
