c89e519ad6ca61da397ea8ebb6b69aa8f645c006
Matthew Rooke <matthew.rooke@my.jcu.edu.au>
2019-10-23 21:15:12 +1000

    Fix code in DebugSixteen2

--- src/week12/Debuggers/FixDebugSixteen2.java
+++ src/week12/Debuggers/FixDebugSixteen2.java
@@ -1,4 +1,5 @@
-// The panel should change colorwhen user's mouse enters or exits it
+package week12.Debuggers;
+// The panel should change color when user's mouse enters or exits it
 // A circle should appear wherever the user clicks
 // The circle becomes smaller when the user double-clicks
 
@@ -7,32 +8,33 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class DebugSixteen2 extends JPanel implements MouseListener {
+public class FixDebugSixteen2 extends JPanel implements MouseListener {
     int x, y;
     int size;
 
-    public DebugSixteen2() {
-        addMouseListener();
+    public FixDebugSixteen2() {
+        addMouseListener(this);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.add(new DebugSixteen2());
+        frame.add(new FixDebugSixteen2());
         frame.setSize(250, 150);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         x = e.getX();
-        y = e.gety();
+        y = e.getY();
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         if (e.getClickCount() == 2)
-            size = 20;
+            size = 10;
         else
             size = 20;
         repaint();
@@ -45,6 +47,7 @@ public class DebugSixteen2 extends JPanel implements MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {
+        setBackground(Color.WHITE);
     }
 
     @Override
