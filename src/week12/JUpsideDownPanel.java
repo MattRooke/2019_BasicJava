c3028fb82e6a294ea38b2d6a1fe66ef7871f4344
Matthew Rooke <matthew.rooke@my.jcu.edu.au>
2019-10-23 18:29:35 +1000

    Add code for JUpsideDownPanel

---
+++ src/week12/JUpsideDownPanel.java
@@ -0,0 +1,45 @@
+package week12;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JUpsideDownPanel extends JPanel {
+    static int size = 30;
+    static int x = 30;
+    int y = 30;
+
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("UpsideDown");
+        mainFrame.setLayout(new BorderLayout());
+        JUpsideDownPanel mainPanel = new JUpsideDownPanel();
+        JButton phraseButton = new JButton("Flip!");
+        JLabel phrase = new JLabel("My life");
+        phrase.setFont(new Font(" ", Font.PLAIN, 30));
+        mainPanel.add(phrase);
+        mainFrame.add(mainPanel, BorderLayout.CENTER);
+        mainFrame.add(phraseButton, BorderLayout.SOUTH);
+        phraseButton.addActionListener(e -> {
+            if (size == 30) {
+                size = -30;
+                x = 130;
+                mainFrame.repaint();
+            } else {
+                size = 30;
+                x = 30;
+                mainFrame.repaint();
+            }
+
+        });
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
+        mainFrame.pack();
+    }
+
+    public void paint(Graphics g) {
+        Font flipFont = new Font("Trebuchet MS", Font.PLAIN, size);
+        g.setFont(flipFont);
+        g.drawString("My Life", x, y);
+    }
+
+}
